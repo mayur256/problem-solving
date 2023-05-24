efc1755b44b81ede887ee0237d6926240af8e350
Mayur Upadhayay <mayur123a@gmail.com>
2023-05-25 01:06:22 +0530

    chore: Solution for magic square

--- Python/hackerrank/magic_square.py
+++ Python/hackerrank/magic_square.py
@@ -7,10 +7,10 @@ Given 's', convert it into a magic square at minimal cost. Print this cost on a 
 
 Example
 
-$s = [[5, 3, 4], [1, 5, 8], [6, 4, 2]]
+$s = [[9, 3, 4], [1, 5, 8], [6, 4, 2]]
 
 The matrix looks like this:
-5 3 4
+9 3 4
 1 5 8
 6 4 2
 
@@ -25,7 +25,27 @@ This took three replacements at a cost of |5-8| + |8-9| + |4-7| = 7.
 
 def formingMagicSquare(s):
     # Write your code here
-    pass
+    possible_magic_squares = [
+        [6, 1, 8, 7, 5, 3, 2, 9, 4], #0
+        [8, 1, 6, 3, 5, 7, 4, 9, 2], #1
+        [6, 7, 2, 1, 5, 9, 8, 3, 4], #2
+        [8, 3, 4, 1, 5, 9, 6, 7, 2], #3
+        [2, 7, 6, 9, 5, 1, 4, 3, 8], #4
+        [4, 3, 8, 9, 5, 1, 2, 7, 6], #5
+        [2, 9, 4, 7, 5, 3, 6, 1, 8], #6
+        [4, 9, 2, 3, 5, 7, 8, 1, 6], #7
+    ]
 
-s = [[5, 3, 4], [1, 5, 8], [6, 4, 2]]
+    s = [x for y in s for x in y]  # Flattening the input to compare
+
+    costs = list()
+
+    for i in possible_magic_squares:
+        cost = sum( [ abs(x[0] - x[1] ) for x in zip(i, s)  ]  )
+        costs.append((cost))
+
+    return min(sorted(costs, reverse= False))
+
+
+s = [[9, 3, 4], [1, 5, 8], [6, 4, 2]]
 result = formingMagicSquare(s)
