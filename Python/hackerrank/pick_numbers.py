0ad97fe4ed3de461ef76870c60cf7b38b805b554
mayur256 <39913092+mayur256@users.noreply.github.com>
2023-07-04 00:14:25 +0530

    chore: Solutions for pick_numbers

--- Python/hackerrank/pick_numbers.py
+++ Python/hackerrank/pick_numbers.py
@@ -6,3 +6,22 @@ a=[1,1,2,2,4,4,5,5,5]
 
 There are two subarrays meeting the criterion: [1,1,2,2] and [4,4,5,5,5]. The maximum length subarray has 5 elements.
 """
+
+#Solution
+def pickingNumbers(a):
+    # Write your code here
+    a.sort()
+    print(a)
+    maxSubArrLen = 0
+    for i in range(len(a)):
+        subArr=[]
+        subArr.append(a[i])
+        for j in range(i+1,len(a)):
+            if abs(a[i]-a[j])==0 or abs(a[i]-a[j])==1:
+                subArr.append(a[j])
+        maxSubArrLen=max(len(subArr), maxSubArrLen)
+        
+    return maxSubArrLen
+    
+result = pickingNumbers([1, 2, 2, 3, 1, 2])
+print(result)
